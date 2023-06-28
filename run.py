a8d141bd335e56edd3083786b23981a66a20229a
Prathamesh Dhande <prathameshdhande534@gmail.com>
2023-06-28 08:43:19 +0530

    files made for render

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-from gevent.pywsgi import WSGIServer
 from feedback import create_app
 
 app = create_app()
-http_server = WSGIServer(("127.0.0.1", 8000), app)
-http_server.serve_forever()
+
+
+def run():
+    app.run()
+
+
+if __name__ == '__main__':
+    run()
